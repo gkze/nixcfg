eeb6f4ade3b8b553a11bdd257ca590ae9f8cf219
gkze <george.kontridze@gmail.com>
2023-12-29 15:48:15 -0800

    macOS updates

--- nix/homebrew.nix
+++ nix/homebrew.nix
@@ -2,7 +2,7 @@
   # Homebrew & mas
   homebrew = {
     # Only manage Homebrew when underlying system is macOS
-    # (Maybe try on NixOS in the future)
+    # (Maybe try on other OSes in the future)
     enable = pkgs.stdenv.isDarwin;
 
     # Stay updated & clean up things that are not defined below
@@ -12,44 +12,42 @@
     taps = [ "gkze/gkze" "homebrew/cask-fonts" "homebrew/cask-versions" ];
 
     # Homebrew Formulae
-    brews = [ "bash-language-server" "stars" ];
+    brews = [ ];
 
     # Homebrew Casks. They've been split out to what I believe to be
     # cross-platform vs. macOS-only casks (in case we'd like to use Homebrew
     # outside of macOS in the future for whatever reason)
+    # TODO: refactor into os / device / profile based installation
     casks = [
-      "alacritty"
-      "beekeeper-studio"
-      "beeper"
-      "brave-browser-beta"
-      "discord"
-      "docker"
-      "element"
-      "figma"
-      "firefox"
-      "gimp"
-      "github"
-      "google-chrome"
-      "google-drive"
-      "httpie"
-      "low-profile"
-      "messenger"
-      "musicbrainz-picard"
-      "nordvpn"
-      "openlens"
-      "signal"
-      "skype"
-      "slack"
-      "soulseek"
-      "spotify"
-      "telegram"
-      "vagrant"
-      "visual-studio-code-insiders"
-      "vlc"
-      "webtorrent"
-      "whatsapp"
-      "zoom"
+      # "discord"
+      # "docker"
+      # "figma"
+      # "firefox"
+      # "gimp"
+      # "github"
+      # "google-chrome"
+      # "google-drive"
+      # "httpie"
+      # "low-profile"
+      # "messenger"
+      # "musicbrainz-picard"
+      # "nordvpn"
+      # "openlens"
+      # "signal"
+      # "skype"
+      # "slack"
+      # "soulseek"
+      # "spotify"
+      # "telegram"
+      # "vagrant"
+      # "visual-studio-code-insiders"
+      # "vlc"
+      # "webtorrent"
+      # "whatsapp"
+      # "zoom"
     ] ++ (lib.optionals pkgs.stdenv.isDarwin [
+      "brave-browser-beta"
+      "beekeeper-studio"
       "aerial"
       "appcleaner"
       "codeedit"
