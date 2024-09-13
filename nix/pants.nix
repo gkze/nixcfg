76e4720c022b05d94124fd2ab9156be3fbeecc7e
gkze <george.kontridze@gmail.com>
2024-09-12 18:27:10 -0700

    updates

--- nix/pants.nix
+++
@@ -1,6 +0,0 @@
-{ rustPlatform, fetchFromGitHub }:
-let version = "2.20.1"; in rustPlatform.buildRustCrate {
-  pname = "pants-engine";
-  inherit version;
-  src = fetchFromGitHub { owner = "pantsbuild"; repo = "pants"; };
-}
