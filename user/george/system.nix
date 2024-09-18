43d6d17d5290333e628b87407ae674262f3073b2
gkze <george.kontridze@gmail.com>
2024-09-17 22:03:45 -0700

    updates

--- user/george/system.nix
+++ user/george/system.nix
@@ -9,22 +9,24 @@ in
   imports = [
     {
       darwin = { };
-      linux = { networking.networkmanager.insertNameservers = [ "127.0.0.1" ]; };
+      linux = {
+        services.coredns = {
+          enable = false;
+          config = ''
+            . {
+              forward . 1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4
+              cache
+            }
+
+            local {
+              template IN A {
+                answer "{{ .Name }} 0 IN A 127.0.0.1"
+              }
+            }
+          '';
+        };
+        networking.networkmanager.insertNameservers = [ "127.0.0.1" ];
+      };
     }.${kernel}
   ];
-  services.coredns = {
-    enable = false;
-    config = ''
-      . {
-        forward . 1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4
-        cache
-      }
-
-      local {
-        template IN A {
-          answer "{{ .Name }} 0 IN A 127.0.0.1"
-        }
-      }
-    '';
-  };
 }
