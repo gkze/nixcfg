e3cc5d362c9dc1570c7614fcb81b3168e09afb29
gkze <george.kontridze@gmail.com>
2025-02-16 16:42:32 -0800

    updates

---
+++ modules/darwin/town-brew-apps.nix
@@ -0,0 +1,11 @@
+{
+  homebrew = {
+    masApps = {
+      "Microsoft Word" = 462054704;
+      "Microsoft Outlook" = 985367838;
+      "Microsoft OneNote" = 784801555;
+      "Microsoft Excel" = 462058435;
+      "Microsoft PowerPoint" = 462062816;
+    };
+  };
+}
