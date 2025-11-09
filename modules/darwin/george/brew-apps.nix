ea52c74f7ac3fed5c58a3058f4da0c9eff737783
gkze <george.kontridze@gmail.com>
2025-11-08 19:29:23 -0800

    updates

--- modules/darwin/george/brew-apps.nix
+++ modules/darwin/george/brew-apps.nix
@@ -34,7 +34,7 @@
     ];
     masApps = {
       "AdGuard for Safari" = 1440147259;
-      "Amazon Kindle" = 302584613;
+      # "Amazon Kindle" = 302584613;
       "Apple Configurator" = 1037126344;
       "JSONPeep" = 1458969831;
       "Shazam: Identify Songs" = 897118787;
